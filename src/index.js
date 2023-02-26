3d1b0dc7751463d356a364337a13ca0959e238eb
Raman Kanapeshka <raman.kanapeshka@cloudcommercegroup.com>
2023-02-26 21:21:44 +0100

    make function async

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import './style.css';
 import Icon from './icon.png';
 
-function getComponent() {
-    return import('lodash')
-    .then(({ default: _ }) => {
-      const element = document.createElement('div');
+async function getComponent() {
+  const element = document.createElement('div');
+  const { default: _ } = await import('lodash');
 
-      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-      // Add the image to our existing div.
-      const myIcon = new Image();
-      myIcon.src = Icon;
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
 
-      element.appendChild(myIcon);
-    
-      return element;
-    })
-    .catch((error) => 'An error occurred while loading the component');
-  }
+  element.appendChild(myIcon);
+
+  return element;
+}
   
-  getComponent().then((component) => {
-    document.body.appendChild(component);
-  });
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
   
