7991e03fc00becd8f37fd37e9191b8e6cad7816e
Raman Kanapeshka <raman.kanapeshka@cloudcommercegroup.com>
2023-02-26 19:47:11 +0100

    remove licence file from build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -6,5 +7,16 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development'
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+        terserOptions: {
+            format: {
+                comments: false,
+            },
+        },
+        extractComments: false,
+    })],
+},
+  mode: 'development',
 };
