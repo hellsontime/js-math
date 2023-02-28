2c25ccb01416d862ac98378de437b5c32f57cb4f
Raman Kanapeshka <raman.kanapeshka@cloudcommercegroup.com>
2023-03-01 00:30:30 +0100

    fix all files according to linters

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import './style.css';
-import Icon from './icon.png';
-import printMe from './print.js';
+import "./style.css";
+import Icon from "./icon.png";
+import printMe from "./print";
 
 async function getComponent() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-  const { default: _ } = await import('lodash');
+  const element = document.createElement("div");
+  const btn = document.createElement("button");
+  const { default: _ } = await import("lodash");
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
 
   // Add the image to our existing div.
   const myIcon = new Image();
@@ -16,16 +16,14 @@ async function getComponent() {
   element.appendChild(myIcon);
 
   // Test button
-  btn.innerHTML = 'Click me and check the console!';
+  btn.innerHTML = "Click me and check the console!";
   btn.onclick = printMe;
 
   element.appendChild(btn);
 
-
   return element;
 }
-  
+
 getComponent().then((component) => {
   document.body.appendChild(component);
 });
-  
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,61 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
     index: {
-      import: './src/index.js',
-      dependOn: 'shared',
+      import: "./src/index.js",
+      dependOn: "shared",
     },
     print: {
       import: "./src/print.js",
-      dependOn: 'shared',
+      dependOn: "shared",
     },
-    shared: 'lodash',
+    shared: "lodash",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: "Output Management",
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin({
+    minimizer: [
+      new TerserPlugin({
         terserOptions: {
-            format: {
-                comments: false,
-            },
+          format: {
+            comments: false,
+          },
         },
         extractComments: false,
-    })],
-    runtimeChunk: 'single',
+      }),
+    ],
+    runtimeChunk: "single",
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
     },
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
-  mode: 'development',
+  mode: "development",
 };
