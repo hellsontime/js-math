93bc6327dc0de66076de59e8787d58b710ebbf73
Raman Kanapeshka <raman.kanapeshka@cloudcommercegroup.com>
2023-03-05 21:25:45 +0100

    something

--- webpack.config.js
+++ webpack.config.js
@@ -4,62 +4,62 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: {
-      import: './src/index.js'
-    },
-    print: {
-      import: './src/js/print.js'
-    }
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      hash: true,
-      template: './src/index.html',
-      filename: './index.html'
-    })
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          format: {
-            comments: false
-          }
+    entry: {
+        index: {
+            import: './src/index.js'
         },
-        extractComments: false
-      })
+        print: {
+            import: './src/js/print.js'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: './src/index.html',
+            filename: './index.html'
+        })
     ],
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource'
-      }
-    ]
-  },
-  mode: 'development'
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false
+                    }
+                },
+                extractComments: false
+            })
+        ],
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    mode: 'development'
 };
