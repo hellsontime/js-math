dc50fbd73155acdf65f9912ce7fbf5c07f10b9cf
Raman Kanapeshka <raman.kanapeshka@cloudcommercegroup.com>
2023-03-15 00:01:57 +0100

    implement key click tracker

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@
 import './scss/style.scss';
 import '@fortawesome/fontawesome-free/js/all';
 import '@fortawesome/fontawesome-free/css/all.css';
-import { toggleNightMode, checkNightMode } from './js/mode-switcher';
+import { toggleNightMode, checkNightMode } from './js/modeSwitcher';
+import catchAction from './js/input';
 
 async function getComponent() {
-    window.onload = () => checkNightMode();
+    window.addEventListener('load', checkNightMode());
     const modeSwitchBtn = document.querySelector('#modeSwitch');
     modeSwitchBtn.addEventListener('click', () => toggleNightMode());
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach((key) => {
+        key.addEventListener('click', () =>
+            catchAction(key.getAttribute('key'), key.getAttribute('value'))
+        );
+    });
 }
 
 getComponent().then((component) => {
