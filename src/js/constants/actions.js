9038e0c866aef3f5a54eeea21a003750ac573cc3
Raman Kanapeshka <raman.kanapeshka@cloudcommercegroup.com>
2023-03-15 00:02:48 +0100

    implement actions constant

---
+++ src/js/constants/actions.js
@@ -0,0 +1,14 @@
+import { allClear, changeSign, percent } from '../operations/baseOperations';
+import { plus, minus, multiply, division } from '../operations/baseCalculations';
+
+const actions = {
+    allClear,
+    changeSign,
+    percent,
+    plus,
+    minus,
+    multiply,
+    division
+};
+
+export default actions;
